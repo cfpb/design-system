97cf29b9eb90d1a194f8df5491ceb928ab8e76b1
Catherine Farman <Catherine.Farman@cfpb.gov>
2019-07-03 16:25:25 -0400

    add js to insert anchor link next to h3 headings

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -29,3 +29,34 @@ toggleButton.addEventListener( 'click', ev => {
     toggleButton.setAttribute( 'data-code-hidden', 'true' );
   }
 } );
+
+/**
+ * create permalink elements for easy copy/paste of anchor links at every h3 heading
+ * @todo: for h4 headings too?
+ */
+function addPermalinks( ) {
+  const headings = document.querySelectorAll( '.content_main h3' );
+
+  for ( let i = 0; i < headings.length; i++ ) {
+    const heading = headings[i];
+    const href = heading.getAttribute( 'id' );
+    if ( href ) {
+      // create href with id of heading
+      const link = document.createElement( 'a' );
+      link.setAttribute( 'href', '#' + href );
+      
+      // @todo: use icon-copy.svg
+      link.innerHTML = ' <small>Copy link to #' + href + ' section of this page</small>';
+
+
+      // append to heading
+      heading.appendChild( link );
+    }
+
+  }
+
+  // @todo: copy href to clipboard on click
+
+}
+
+addPermalinks( );

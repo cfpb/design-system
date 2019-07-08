fd2773f4aba48cf3f88c3b8a5fead01b9b91a810
Catherine Farman <Catherine.Farman@cfpb.gov>
2019-07-08 13:10:02 -0400

    add listener to get anchor link href on click

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -31,10 +31,10 @@ toggleButton.addEventListener( 'click', ev => {
 } );
 
 /**
- * create permalink elements for easy copy/paste of anchor links on specified heading elements 
+ * Create permalink elements for easy copy/paste of anchor links on specified heading elements.
+ * @param {HTMLNode} headings - List of target heading DOM elements, e.g. all <h3>'s
  */
 function addPermalinks( headings ) {
-
   const iconLink = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 929.5 1200" class="cf-icon-svg"><path d="M894.4 702.8L731.3 539.7c-46.9-46.8-122.8-46.8-169.7 0l-34.9 34.9-53.2-53.2 34.4-34.4c46.8-46.9 46.8-122.8 0-169.7L344.8 154.2c-46.9-46.8-122.8-46.8-169.7 0l-140 140c-46.8 46.9-46.8 122.8 0 169.7L198.2 627c46.9 46.8 122.8 46.8 169.7 0l34.9-34.9 53.2 53.2-34.4 34.4c-46.8 46.9-46.8 122.8 0 169.7l163.1 163.1c46.9 46.8 122.8 46.8 169.7 0l140-140c46.8-46.9 46.8-122.8 0-169.7zM297.2 556.2c-7.9 7.7-20.4 7.7-28.3 0l-163.1-163c-7.7-7.9-7.7-20.4 0-28.3l140-140c7.9-7.7 20.4-7.7 28.3 0l163 163.1c7.7 7.9 7.7 20.4 0 28.3l-34.4 34.4-55.5-55.5c-19.6-19.4-51.3-19.3-70.7.3-19.3 19.5-19.3 50.9 0 70.4l55.6 55.6-34.9 34.7zm526.5 245.6l-140 140c-7.9 7.7-20.4 7.7-28.3 0L492.3 778.7c-7.7-7.9-7.7-20.4 0-28.3l34.4-34.4 55.5 55.5c19.4 19.6 51.1 19.7 70.7.3s19.7-51.1.3-70.7l-.3-.3-55.5-55.5 34.9-34.9c7.9-7.7 20.4-7.7 28.3 0l163.1 163.1c7.7 7.9 7.7 20.4 0 28.3z"/></svg>';
 
   for ( let i = 0; i < headings.length; i++ ) {
@@ -44,6 +44,7 @@ function addPermalinks( headings ) {
       // create href with id of heading
       const link = document.createElement( 'a' );
       link.setAttribute( 'href', '#' + href );
+      link.setAttribute( 'data-anchor-link', '' );
       link.innerHTML = ' ' + iconLink + ' <small class="u-visually-hidden">Copy link to #' + href + ' section of this page</small>';
       // Title attribute is only useful as a hint to sighted users, screen readers need the actual link text above.
       link.setAttribute( 'title', 'Copy link to #' + href + ' section of this page' );
@@ -52,9 +53,27 @@ function addPermalinks( headings ) {
     }
 
   }
+  /**
+   * Add event listener to copy on click the anchor link href value for permalink icons next to headings.
+   */
+  const permalinks = document.querySelectorAll( '[data-anchor-link]' );
 
-  // @todo: copy href to clipboard on click
+  permalinks.forEach(
+    permalink => permalink.addEventListener( 'click', ev => {
+      ev.preventDefault();
+      console.log( ev.currentTarget.href );
+      copyAnchorLink( );
+    } )
+  );
 
+}
+
+/**
+ * Get href value and copy to clipboard
+ */
+function copyAnchorLink( ) {
+  // do the copy
+  console.log( 'do the copy' );
 }
 
 const h3Els = document.querySelectorAll( '.content_main h3' );

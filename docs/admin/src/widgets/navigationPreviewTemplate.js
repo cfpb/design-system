5c6df7cff966e9c18e39a4258ed83c93d41dbd58
Catherine Farman <Catherine.Farman@cfpb.gov>
2019-07-18 16:47:56 -0400

    add other page data to data object

--- docs/admin/src/widgets/navigationPreviewTemplate.js
+++ docs/admin/src/widgets/navigationPreviewTemplate.js
@@ -4,20 +4,27 @@ import marked from 'marked';
 import template from '../../../_includes/sidebar-preview-pane.html';
 
 export const Preview = props => {
-  const site = {
-    data: {
-      navigation: props.entry.toJS().data
+  const data = {
+    page: {
+      section: 'all',
+      title: 'Preview'
+    },
+    site: {
+      baseurl: 'https://cfpb.github.io/design-system',
+      data: {
+        navigation: props.entry.toJS().data
+      }
     }
   };
 
   window.previewProps = props;
   window.previewReact = React;
 
-  window.previewData = site;
+  window.previewData = data;
 
   return (
     <div>
-      <ReactLiquid template={template} site={site} html />
+      <ReactLiquid template={template} data={data} html />
     </div>
   );
 };

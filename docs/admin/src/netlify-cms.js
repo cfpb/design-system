92625c91c78f40922c8b19ed72222a03a9c143c9
Catherine Farman <Catherine.Farman@cfpb.gov>
2019-08-06 12:44:05 -0400

    add new template folers, added all new collections

--- docs/admin/src/netlify-cms.js
+++ docs/admin/src/netlify-cms.js
@@ -3,7 +3,10 @@ import { default as componentsPreviewTemplate } from './widgets/componentsPrevie
 import { Preview as navPreview } from './widgets/navigationPreviewTemplate';
 
 CMS.registerPreviewTemplate( 'getting-started', componentsPreviewTemplate );
+CMS.registerPreviewTemplate( 'foundation', componentsPreviewTemplate );
 CMS.registerPreviewTemplate( 'components', componentsPreviewTemplate );
+CMS.registerPreviewTemplate( 'templates', componentsPreviewTemplate );
 CMS.registerPreviewTemplate( 'navigation', navPreview );
+
 
 CMS.registerPreviewStyle( '/design-system/dist/css/main.css' );

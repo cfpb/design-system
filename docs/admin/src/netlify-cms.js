5edd53ceb53e8896b3f0e90997517e60749bcb23
Catherine Farman <Catherine.Farman@cfpb.gov>
2019-08-06 12:16:33 -0400

    register preview pane template for getting-started collection

--- docs/admin/src/netlify-cms.js
+++ docs/admin/src/netlify-cms.js
@@ -2,6 +2,7 @@ import CMS from '@cfpb/netlify-cms';
 import { default as componentsPreviewTemplate } from './widgets/componentsPreviewTemplate';
 import { Preview as navPreview } from './widgets/navigationPreviewTemplate';
 
+CMS.registerPreviewTemplate( 'getting-started', componentsPreviewTemplate );
 CMS.registerPreviewTemplate( 'components', componentsPreviewTemplate );
 CMS.registerPreviewTemplate( 'navigation', navPreview );
 

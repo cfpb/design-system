4c22d23641b772ab051193b4d4da3aaac227008b
Chris Contolini <contolini@users.noreply.github.com>
2020-04-15 14:53:17 -0400

    Initial browser test implementation using webdriver.io
    Sets up webdriver.io with a simple test for the hide/show code snippet
    toggling functionality. Adds two commands: `yarn webdriver` and `yarn
    webdriver-sauce`. The latter requires your sauce credentials via env
    variables.

---
+++ test/browser/docs/code-snippets.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-undef */
+
+describe( 'the code snippet toggling feature', () => {
+
+  let codeToggleButton;
+  let codeSnippets;
+  let areCodeSnippetsVisible;
+
+  before( () => {
+    browser.url( 'http://localhost:4000/design-system/components/buttons' );
+    browser.setWindowSize( 1024, 768 );
+    codeToggleButton = $( '#toggle-code-btn' );
+    codeSnippets = [ ...$$( '[data-toggle-code]' ) ];
+    areCodeSnippetsVisible = () => codeSnippets.some( snippet => snippet.isDisplayed() );
+  } );
+
+  it( 'should show code snippets by default', () => {
+    expect( areCodeSnippetsVisible() ).toBeTruthy();
+  } );
+
+  it( 'should hide code snippets when toggle button is clicked', () => {
+    codeToggleButton.click();
+    expect( codeToggleButton.getText() ).toContain( 'Show' );
+    expect( areCodeSnippetsVisible() ).toBeFalsy();
+  } );
+
+  it( 'should re-show code snippets when toggle button is clicked again', () => {
+    codeToggleButton.click();
+    expect( codeToggleButton.getText() ).toContain( 'Hide' );
+    expect( areCodeSnippetsVisible() ).toBeTruthy();
+  } );
+
+} );

9aca3e8eae058b73fb2a2abee04729ae037fb796
CFPBot <CFPBot@users.noreply.github.com>
2020-04-28 05:55:59 +0000

    Update GitHub Pages

--- admin/src/netlify-cms.js
+++ admin/src/netlify-cms.js
@@ -1,5 +1,6 @@
 import CMS from 'netlify-cms';
 import { default as componentsPreviewTemplate } from './widgets/componentsPreviewTemplate';
+import { default as genericPreviewTemplate } from './widgets/genericPreviewTemplate';
 import { Preview as navPreview } from './widgets/navigationPreviewTemplate';
 
 CMS.registerPreviewTemplate( 'getting-started', componentsPreviewTemplate );
@@ -7,6 +8,6 @@ CMS.registerPreviewTemplate( 'foundation', componentsPreviewTemplate );
 CMS.registerPreviewTemplate( 'components', componentsPreviewTemplate );
 CMS.registerPreviewTemplate( 'templates', componentsPreviewTemplate );
 CMS.registerPreviewTemplate( 'navigation', navPreview );
-
+CMS.registerPreviewTemplate( 'home', genericPreviewTemplate );
 
 CMS.registerPreviewStyle( '/design-system/dist/css/main.css' );

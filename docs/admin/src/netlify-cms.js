c86af6e14a4acfd0239c5a746671fb43da4c65ec
Chris Contolini <contolini@users.noreply.github.com>
2020-05-08 13:38:35 -0400

    Remove navigation preview widget

--- docs/admin/src/netlify-cms.js
+++ docs/admin/src/netlify-cms.js
@@ -1,13 +1,11 @@
 import CMS from 'netlify-cms';
 import { default as componentsPreviewTemplate } from './widgets/componentsPreviewTemplate';
 import { default as genericPreviewTemplate } from './widgets/genericPreviewTemplate';
-import { Preview as navPreview } from './widgets/navigationPreviewTemplate';
 
 CMS.registerPreviewTemplate( 'getting-started', componentsPreviewTemplate );
 CMS.registerPreviewTemplate( 'foundation', componentsPreviewTemplate );
 CMS.registerPreviewTemplate( 'components', componentsPreviewTemplate );
 CMS.registerPreviewTemplate( 'templates', componentsPreviewTemplate );
-CMS.registerPreviewTemplate( 'navigation', navPreview );
 CMS.registerPreviewTemplate( 'home', genericPreviewTemplate );
 
 CMS.registerPreviewStyle( '/design-system/dist/css/main.css' );

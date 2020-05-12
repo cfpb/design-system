fbb4f51fa869dc73c2656b54c4c1c144b014ddf9
Chris Contolini <contolini@users.noreply.github.com>
2020-05-12 15:06:02 -0400

    Add browser test for component page previews

--- test/browser/docs/netlify-cms.js
+++ test/browser/docs/netlify-cms.js
@@ -1,3 +1,4 @@
+/* eslint-disable handle-callback-err */
 /* eslint-disable no-undef */
 
 describe( 'Netlify CMS', () => {
@@ -5,43 +6,79 @@ describe( 'Netlify CMS', () => {
   let loginButton;
 
   beforeEach( () => {
-    browser.url( 'http://localhost:4000/design-system/admin/#/collections/generic-pages/entries/home' );
-    loginButton = browser.react$( 'LoginButton' );
+    browser.reloadSession();
   } );
 
-  afterEach( () => {
-    browser.execute( () => {
-      localStorage.clear();
-      sessionStorage.clear();
+  describe( 'Editing the homepage', () => {
+
+    beforeEach( () => {
+      browser.url( 'http://localhost:4000/design-system/admin/#/collections/generic-pages/entries/home' );
+      loginButton = browser.react$( 'LoginButton' );
     } );
-    browser.refresh();
+
+    it( 'should load the login page with a login button', () => {
+      loginButton.waitForDisplayed();
+      expect( loginButton ).toExist();
+    } );
+
+    it( 'should allow the user to log in', () => {
+      loginButton.waitForDisplayed();
+      loginButton.click();
+      const editorContainer = browser.react$( 'EditorContainer' );
+      editorContainer.waitForDisplayed();
+      expect( editorContainer ).toExist();
+    } );
+
+    it( 'should properly render a preview of a page', () => {
+      loginButton.waitForDisplayed();
+      loginButton.click();
+      // The homepage's body field
+      const pageBodyField = $( '#nc-root #body-field-1' );
+      pageBodyField.waitForDisplayed();
+      pageBodyField.clearValue();
+      pageBodyField.setValue( 'browser tests are fun' );
+      // The preview pane is an iframe
+      browser.switchToFrame( $( 'iframe' ) );
+      const previewPane = $( 'body' );
+      expect( previewPane ).toHaveTextContaining( 'browser tests are fun' );
+    } );
+
   } );
 
-  it( 'should load the login page with a login button', () => {
-    loginButton.waitForDisplayed();
-    expect( loginButton ).toExist();
-  } );
+  describe( 'Editing a component page', () => {
 
-  it( 'should allow the user to log in', () => {
-    loginButton.waitForDisplayed();
-    loginButton.click();
-    const editorContainer = browser.react$( 'EditorContainer' );
-    editorContainer.waitForDisplayed();
-    expect( editorContainer ).toExist();
-  } );
+    beforeEach( () => {
+      browser.url( 'http://localhost:4000/design-system/admin/#/collections/components/entries/buttons' );
+      loginButton = browser.react$( 'LoginButton' );
+    } );
 
-  it( 'should properly render a preview of a page', () => {
-    loginButton.waitForDisplayed();
-    loginButton.click();
-    // The homepage's body field
-    const pageTitleField = $( '#nc-root #body-field-1' );
-    pageTitleField.waitForDisplayed();
-    pageTitleField.clearValue();
-    pageTitleField.setValue( 'browser tests are fun' );
-    // The preview pane is an iframe
-    browser.switchToFrame( $( 'iframe' ) );
-    const previewPane = $( 'body' );
-    expect( previewPane ).toHaveTextContaining( 'browser tests are fun' );
+    it( 'should load the login page with a login button', () => {
+      loginButton.waitForDisplayed();
+      expect( loginButton ).toExist();
+    } );
+
+    it( 'should allow the user to log in', () => {
+      loginButton.waitForDisplayed();
+      loginButton.click();
+      const editorContainer = browser.react$( 'EditorContainer' );
+      editorContainer.waitForDisplayed();
+      expect( editorContainer ).toExist();
+    } );
+
+    it( 'should properly render a preview of a page', () => {
+      loginButton.waitForDisplayed();
+      loginButton.click();
+      // The button page's title field
+      const pageTitleField = $( '#nc-root #title-field-1' );
+      pageTitleField.waitForDisplayed();
+      pageTitleField.clearValue();
+      pageTitleField.setValue( 'component pages are the best' );
+      // The preview pane is an iframe
+      browser.switchToFrame( $( 'iframe' ) );
+      const previewPane = $( 'body' );
+      expect( previewPane ).toHaveTextContaining( 'component pages are the best' );
+    } );
+
   } );
 
 } );

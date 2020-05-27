a9853f414ffe5c6efc457fe5ff9eb56918e9deeb
Chris Contolini <contolini@users.noreply.github.com>
2020-05-27 13:41:14 -0400

    Move netlify build script into separate file for clarity

---
+++ scripts/build-netlify-pr-preview.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -eu
+
+cd docs/
+yarn
+yarn run build-netlify
+
+# The design system website lives at cfpb.github.io/design-system
+# To simulate this with the PR preview websites we put the compiled
+# Jekyll site into a design-system/ sub directory
+mv _site/ _site-tmp/
+mkdir -p _site/design-system
+mv _site-tmp/* _site/design-system/

56a909705d2c1cd7f187fbb155860c66e8ef1be5
Chris Contolini <contolini@users.noreply.github.com>
2020-06-09 12:08:20 -0400

    Only show CMS interstitial page once every two weeks

--- docs/assets/js/interstitial.js
+++ docs/assets/js/interstitial.js
@@ -12,10 +12,20 @@ function getUrlParameter( name ) {
 const page = getUrlParameter( 'page' );
 
 if ( page && window.PAGES_WHITELIST[page] ) {
+  // Skip the interstitial page if they've seen it within the past two weeks
+  if ( localStorage.getItem( 'cms-directions-last-seen' ) ) {
+    const now = new Date();
+    const lastSeen = Date.parse( localStorage.getItem( 'cms-directions-last-seen' ) );
+    const daysSinceDirectionsSeen = ( now - lastSeen ) / 1000 / 60 / 60 / 24;
+    if ( daysSinceDirectionsSeen <= 14 ) {
+      window.location.replace( window.PAGES_WHITELIST[page] );
+    }
+  }
   document.querySelector( '[data-interstitial-notification]' ).classList.add( 'm-notification__visible' );
   document.querySelector( '[data-interstitial-next-step]' ).classList.remove( 'u-hidden' );
   document.querySelector( '[data-interstitial-redirect-button]' ).setAttribute( 'href', window.PAGES_WHITELIST[page] );
   document.querySelectorAll( '[data-interstitial-page-name]' ).forEach( el => {
     el.textContent = `"${ page }"`;
   } );
+  localStorage.setItem( 'cms-directions-last-seen', new Date() );
 }

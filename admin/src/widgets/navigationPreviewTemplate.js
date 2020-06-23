28303b80dba9281c590261803c3df4eda9345ada
CFPBot <CFPBot@users.noreply.github.com>
2020-06-23 19:37:02 +0000

    Update GitHub Pages

--- admin/src/widgets/navigationPreviewTemplate.js
+++ admin/src/widgets/navigationPreviewTemplate.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 import { ReactLiquid, liquidEngine } from 'react-liquid';
+import slugify from 'slugify';
 import template from '../../../_includes/sidebar.html';
 
 export default class Preview extends Component {
+
+  componentDidMount() {
+    liquidEngine.registerFilter( 'size', initial => initial.length || 1 );
+    liquidEngine.registerFilter( 'relative_url', initial => `/design-system/${ initial }` );
+    liquidEngine.registerFilter( 'slugify', initial => slugify( initial || '', { lower: true } ) );
+  }
 
   render() {
     const data = {

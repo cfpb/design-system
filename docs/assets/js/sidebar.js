6ac71aa1c9bcdf3c83c51dfb3ddd1715fa02baa3
Anselm Bradford <anselmbradford@users.noreply.github.com>
2020-07-08 13:57:22 -0400

    Re-work secondary navigation

---
+++ docs/assets/js/sidebar.js
@@ -0,0 +1,27 @@
+function init() {
+  const secondaryNavCat = document.querySelector( '.ds-nav-container' );
+
+  /**
+   * Test the viewport size and set whether the test passes on the instance.
+   */
+  function handleViewportChange() {
+    if ( window.innerWidth < 601 ) {
+      secondaryNavCat.removeAttribute( 'open' );
+
+    } else {
+      secondaryNavCat.setAttribute( 'open', 'open' );
+    }
+  }
+
+  // Check viewport state on page load.
+  handleViewportChange();
+
+  // Add event listener for checking viewport state on window resize.
+  window.addEventListener( 'resize', () => {
+    handleViewportChange();
+  } );
+}
+
+export default {
+  init
+};

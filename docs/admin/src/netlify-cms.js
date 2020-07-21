488afd57c0ce4277c14dd43324337f9b479c8fbc
Chris Contolini <contolini@users.noreply.github.com>
2020-07-21 08:34:56 -0700

    Add link to CMS instructions page for new users (#837)
    * Add link to CMS instructions page for new users
    
    Creates a new admin widget that includes a link to our CMS instructions
    page. It's identical to our string widget but includes a link underneath
    it.
    
    This approach adds some redundant code but adding/editing widgets is the
    sustainable method of modifying the CMS's left-hand editing pane.
    The alternative would be forking netlify-cms and tweaking its source code
    which is a maintenance headache.
    
    * Add rel attribute to CMS instructions link

--- docs/admin/src/netlify-cms.js
+++ docs/admin/src/netlify-cms.js
@@ -2,11 +2,13 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import CMS from 'netlify-cms';
 import StringTrimmedControl from './widgets/StringTrimmed';
+import StringWithInstructionsControl from './widgets/StringWithInstructions';
 import genericPreviewTemplate from './widgets/genericPreviewTemplate';
 import navigationPreviewTemplate from './widgets/navigationPreviewTemplate';
 import pagePreviewTemplate from './widgets/pagePreviewTemplate';
 
 CMS.registerWidget( 'string-trimmed', StringTrimmedControl, 'string' );
+CMS.registerWidget( 'string-with-instructions', StringWithInstructionsControl, 'string' );
 
 CMS.registerPreviewTemplate( 'special-pages', genericPreviewTemplate );
 CMS.registerPreviewTemplate( 'pages', pagePreviewTemplate );

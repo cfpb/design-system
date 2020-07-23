7318f214175857acc23cb5a271cc1bb2063729aa
Andy Chosak <andy.chosak@cfpb.gov>
2020-07-23 15:20:29 -0400

    Build docs into design-system subdirectory
    Currently we build the docs under docs/_site. Instead let's build them
    under docs/_site/design-system, which will simplify a couple of
    downstream consumers.

--- scripts/build-netlify-pr-preview.sh
+++ scripts/build-netlify-pr-preview.sh
@@ -4,10 +4,3 @@ set -eu
 
 yarn
 yarn run build-netlify
-
-# The design system website lives at cfpb.github.io/design-system
-# To simulate this with the PR preview websites we put the compiled
-# Jekyll site into a design-system/ sub directory
-mv docs/_site/ docs/_site-tmp/
-mkdir -p docs/_site/design-system
-mv docs/_site-tmp/* docs/_site/design-system/

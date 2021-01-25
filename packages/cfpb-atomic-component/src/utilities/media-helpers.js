e60b9950851fe307688d1ba64cfde049b69b13e7
Anselm Bradford <anselmbradford@users.noreply.github.com>
2021-01-25 10:02:43 -0500

    Only initialize multiselect if we're not on a mobile OS

---
+++ packages/cfpb-atomic-component/src/utilities/media-helpers.js
@@ -0,0 +1,24 @@
+/* ==========================================================================
+   Media Helpers.
+   Utilities for working with different screen sizes and operating systems.
+   ========================================================================= */
+
+/**
+ * Query the browser's user agent string to see if it's on a mobile OS.
+ * @returns {boolean} True if on a mobile user agent, false otherwise.
+ */
+function isMobileUserAgent() {
+  const regex = new RegExp(
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i
+  );
+  if ( regex.test( navigator.userAgent ) ) {
+    return true;
+  }
+  return false;
+
+}
+
+// Expose public methods.
+export {
+  isMobileUserAgent
+};

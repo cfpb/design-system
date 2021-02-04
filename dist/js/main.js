84f2e1917164ad675191a7c5c98ed4494be03150
CFPBot <CFPBot@users.noreply.github.com>
2021-02-04 00:31:46 +0000

    Update GitHub Pages

--- dist/js/main.js
+++ dist/js/main.js
@@ -5172,7 +5172,7 @@ function MaxHeightTransition( element ) {
   function init() {
     _baseTransition.init();
 
-    element.style.maxHeight = element.scrollHeight + 'px';
+    refresh();
 
     const _transitionCompleteBinded = _transitionComplete.bind( this );
     _baseTransition.addEventListener(
@@ -5233,6 +5233,13 @@ function MaxHeightTransition( element ) {
   }
 
   /**
+   * Refresh the max height set on the element.
+   */
+  function refresh() {
+    element.style.maxHeight = element.scrollHeight + 'px';
+  }
+
+  /**
    * Remove style attribute.
    * Remove all transition classes, if transition is initialized.
    * @returns {boolean}
@@ -5254,6 +5261,7 @@ function MaxHeightTransition( element ) {
   this.halt = _baseTransition.halt;
   this.isAnimated = _baseTransition.isAnimated;
   this.setElement = _baseTransition.setElement;
+  this.refresh = refresh;
   this.remove = remove;
 
   this.init = init;

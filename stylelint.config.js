86dbdc69780096342ce4f6ae24493fe60df64540
Ans <git@anselmbradford.com>
2022-04-14 17:14:41 -0400

    Update stylelint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,41 +2,32 @@
 at-rule-no-unknown -
   This rule enforces only @ rules that appear in the CSS spec,
   however, @plugin appears in Less, so should be ignored.
-
 declaration-colon-newline-after -
   Turned off because it messes with our format for multi-line declarations.
-
 function-parentheses-space-inside -
   Custom setting that differs from stylelint-config-standard.
-
 rule-empty-line-before -
   Custom setting that differs from stylelint-config-standard.
-
 max-empty-lines -
   Set to 2 since we have existing two line breaks in place. Could be removed.
-
 no-descending-specificity -
   Turned off, but probably shouldn't be.
   TODO: Turn on this rule and see if issues can be fixed.
-
 selector-list-comma-newline-after -
   Turned off because it wraps arguments in Less mixin declarations.
-
 selector-pseudo-element-colon-notation -
   Set to 'single' to support IE8.
   Remove this rule after dropping IE8 CSS support.
 */
 module.exports = {
   'extends': 'stylelint-config-standard',
+  'ignoreFiles': [ 'packages/**/node_modules/**/*.less' ],
   'syntax': 'less',
   'rules': {
     'at-rule-no-unknown': [ true, { ignoreAtRules: 'plugin' } ],
     'declaration-colon-newline-after': null,
     'declaration-empty-line-before': null,
-    'function-name-case': [
-      'lower',
-      { ignoreFunctions: [ 'filter' ]}
-    ],
+    'function-name-case': [ 'lower', { ignoreFunctions: [ 'filter' ]} ],
     'function-parentheses-space-inside': 'always',
     'rule-empty-line-before': [
       'always-multi-line',
@@ -46,7 +37,7 @@ module.exports = {
       }
     ],
     'indentation': [
-      4,
+      2,
       {
         ignore: 'value'
       }

0756434c3cdc35298b4fb8e0fa53dd4c95ec1e1d
Ans <Anselm.Bradford@cfpb.gov>
2022-10-14 14:56:34 -0400

    Run Stylelint

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,8 +1,8 @@
 ## Run prettier. See ignored path in .prettierignore.
-yarn prettier "{docs,packages}/{!_site,**}/*.{js,jsx,ts,tsx,json,md}" --write
+yarn prettier "{docs,packages}/**/*.{js,jsx,ts,tsx,json,md}" --write
 
 ## Run JS linting. See ignored path in .eslintignore.
 yarn eslint ./{docs,packages} --fix
 
 ## Run CSS linting. See ignored path in .stylelintignore.
-yarn stylelint \"{docs,packages}/**/*.{css,less}\" --fix
+yarn stylelint "{docs,packages}/**/*.{css,less}" --fix

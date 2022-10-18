3e00da108bb77e57ef6e2353bf4a3c28da8bebbc
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2022-10-18 16:22:05 -0400

    set perms on and update script

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-# Add required webfonts locally to src/static/fonts/ directory.
-mkdir ./static/
-mkdir ./static/fonts
-cd ./static/fonts
+# Add required webfonts locally to docs/fonts/ directory.
+mkdir ./docs/fonts/
+cd ./docs/fonts
 curl -s 'https://github.com/cfpb/consumerfinance.gov/blob/main/static.in/cfgov-fonts/fonts/2cd55546-ec00-4af9-aeca-4a3cd186da53.woff2?raw=true' --output '1e9892c0-6927-4412-9874-1b82801ba47a.woff'
 curl -s 'https://github.com/cfpb/consumerfinance.gov/blob/main/static.in/cfgov-fonts/fonts/2cd55546-ec00-4af9-aeca-4a3cd186da53.woff2?raw=true' --output '2cd55546-ec00-4af9-aeca-4a3cd186da53.woff2'
 curl -s 'https://github.com/cfpb/consumerfinance.gov/blob/main/static.in/cfgov-fonts/fonts/627fbb5a-3bae-4cd9-b617-2f923e29d55e.woff2?raw=true' --output '627fbb5a-3bae-4cd9-b617-2f923e29d55e.woff2'

6c47239d7130eb763a20f1917d3bc8afd56f4dd0
CFPBot <CFPBot@users.noreply.github.com>
2023-02-16 01:59:53 +0000

    Update GitHub Pages

--- dist/js/main.js
+++ dist/js/main.js
@@ -2371,6 +2371,25 @@ __webpack_require__.r(__webpack_exports__);
 
 /**
  * @param {HTMLElement} element - DOM element.
+ * @param {string} value - Value to check as existing as a JS data-* hook value.
+ * @returns {boolean} True if the data-* hook value exists, false otherwise.
+ */
+function contains(element, value) {
+  if (!element) {
+    return false;
+  }
+  let values = element.getAttribute(_standard_type_js__WEBPACK_IMPORTED_MODULE_0__.JS_HOOK);
+  // If JS data-* hook is not set return immediately.
+  if (!values) {
+    return false;
+  }
+  values = values.split(' ');
+
+  return values.indexOf(value) > -1 ? true : false;
+}
+
+/**
+ * @param {HTMLElement} element - DOM element.
  * @param {string} value - Value to add to the element's JS data-* hook.
  * @returns {string} The value that was added.
  * @throws {Error} If supplied value contains a space,
@@ -2409,25 +2428,6 @@ function remove(element, value) {
   }
 
   return false;
-}
-
-/**
- * @param {HTMLElement} element - DOM element.
- * @param {string} value - Value to check as existing as a JS data-* hook value.
- * @returns {boolean} True if the data-* hook value exists, false otherwise.
- */
-function contains(element, value) {
-  if (!element) {
-    return false;
-  }
-  let values = element.getAttribute(_standard_type_js__WEBPACK_IMPORTED_MODULE_0__.JS_HOOK);
-  // If JS data-* hook is not set return immediately.
-  if (!values) {
-    return false;
-  }
-  values = values.split(' ');
-
-  return values.indexOf(value) > -1 ? true : false;
 }
 
 

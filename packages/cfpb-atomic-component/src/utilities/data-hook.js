488d2533c301eb71daeee918823332ae7f50f36f
Ans <git@anselmbradford.com>
2023-02-15 20:57:18 -0500

    Fix data-hook declaration order

--- packages/cfpb-atomic-component/src/utilities/data-hook.js
+++ packages/cfpb-atomic-component/src/utilities/data-hook.js
@@ -2,6 +2,25 @@ import { JS_HOOK } from './standard-type.js';
 
 /**
  * @param {HTMLElement} element - DOM element.
+ * @param {string} value - Value to check as existing as a JS data-* hook value.
+ * @returns {boolean} True if the data-* hook value exists, false otherwise.
+ */
+function contains(element, value) {
+  if (!element) {
+    return false;
+  }
+  let values = element.getAttribute(JS_HOOK);
+  // If JS data-* hook is not set return immediately.
+  if (!values) {
+    return false;
+  }
+  values = values.split(' ');
+
+  return values.indexOf(value) > -1 ? true : false;
+}
+
+/**
+ * @param {HTMLElement} element - DOM element.
  * @param {string} value - Value to add to the element's JS data-* hook.
  * @returns {string} The value that was added.
  * @throws {Error} If supplied value contains a space,
@@ -40,25 +59,6 @@ function remove(element, value) {
   }
 
   return false;
-}
-
-/**
- * @param {HTMLElement} element - DOM element.
- * @param {string} value - Value to check as existing as a JS data-* hook value.
- * @returns {boolean} True if the data-* hook value exists, false otherwise.
- */
-function contains(element, value) {
-  if (!element) {
-    return false;
-  }
-  let values = element.getAttribute(JS_HOOK);
-  // If JS data-* hook is not set return immediately.
-  if (!values) {
-    return false;
-  }
-  values = values.split(' ');
-
-  return values.indexOf(value) > -1 ? true : false;
 }
 
 export { add, contains, remove };

1e6f2f2c3485bca3cca63f3ba713fbc95a0b3f1b
Joshua Allen <130792942+BrewCityBoy@users.noreply.github.com>
2023-08-03 16:02:47 -0400

    decap install no custom widgets

--- docs/admin/src/netlify-cms.js
+++ docs/admin/src/netlify-cms.js
@@ -1,9 +1,12 @@
-import CMS from 'netlify-cms';
+import CMS from 'netlify-cms-app';
 import { StringTrimmedControl } from './widgets/StringTrimmed.js';
 import { StringWithInstructionsControl } from './widgets/StringWithInstructions.js';
 import genericPreviewTemplate from './widgets/genericPreviewTemplate.js';
 import navigationPreviewTemplate from './widgets/navigationPreviewTemplate.js';
 import pagePreviewTemplate from './widgets/pagePreviewTemplate.js';
+
+// Initialize the CMS object
+CMS.init();
 
 CMS.registerWidget('string-trimmed', StringTrimmedControl);
 CMS.registerWidget('string-with-instructions', StringWithInstructionsControl);
@@ -11,5 +14,7 @@ CMS.registerWidget('string-with-instructions', StringWithInstructionsControl);
 CMS.registerPreviewTemplate('special-pages', genericPreviewTemplate);
 CMS.registerPreviewTemplate('pages', pagePreviewTemplate);
 CMS.registerPreviewTemplate('side-navigation', navigationPreviewTemplate);
+// Now the registry is available via the CMS object.
+CMS.registerPreviewTemplate('my-template', MyTemplate);
 
 CMS.registerPreviewStyle('/design-system/dist/css/main.css');

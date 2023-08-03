f32319b6fc593a6fccadbaeaa1f61e2b3b3ec592
Joshua Allen <130792942+BrewCityBoy@users.noreply.github.com>
2023-08-03 18:20:16 -0400

    remove extra code

--- docs/admin/src/netlify-cms.js
+++ docs/admin/src/netlify-cms.js
@@ -14,7 +14,5 @@ CMS.registerWidget('string-with-instructions', StringWithInstructionsControl);
 CMS.registerPreviewTemplate('special-pages', genericPreviewTemplate);
 CMS.registerPreviewTemplate('pages', pagePreviewTemplate);
 CMS.registerPreviewTemplate('side-navigation', navigationPreviewTemplate);
-// Now the registry is available via the CMS object.
-CMS.registerPreviewTemplate('my-template', MyTemplate);
 
 CMS.registerPreviewStyle('/design-system/dist/css/main.css');

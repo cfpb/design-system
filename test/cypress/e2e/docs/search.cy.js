e90a2ade0c20821ee9448f5afa388a96c504758a
Ans <git@anselmbradford.com>
2024-04-18 13:32:32 -0400

    Add search test

---
+++ test/cypress/e2e/docs/search.cy.js
@@ -0,0 +1,21 @@
+describe('The search feature', () => {
+  beforeEach(() => {
+    cy.visit('/search/');
+  });
+
+  it('should show no search results', () => {
+    cy.get('#search-results').should(
+      'contain',
+      "No search results found for ''.",
+    );
+  });
+
+  it('should show search results', () => {
+    cy.get('#search-box').type('button');
+    cy.get('#search-form button').click();
+    cy.get('#search-results').should(
+      'contain',
+      "5 results for search of 'button'.",
+    );
+  });
+});

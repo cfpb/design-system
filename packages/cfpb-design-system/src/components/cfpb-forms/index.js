68883ebda6bedba02b3a86f17497d22f635e9d4d
Ans <git@anselmbradford.com>
2024-08-22 09:16:37 -0400

    Add initial tag styling

--- packages/cfpb-design-system/src/components/cfpb-forms/index.js
+++ packages/cfpb-design-system/src/components/cfpb-forms/index.js
@@ -9,6 +9,7 @@ export * as labelStyles from './label.scss';
 export * as rangeStyles from './range.scss';
 export * as searchInputStyles from './search-input.scss';
 export * as selectStyles from './select.scss';
+export * as tagStyles from './tag.scss';
 export * as textInputStyles from './text-input.scss';
 
 export { Multiselect } from './multiselect.js';

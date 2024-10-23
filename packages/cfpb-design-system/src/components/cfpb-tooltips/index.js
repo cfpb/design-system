99d7e2e1f053546ac72acf311e6a90dcc2374e20
Chris Contolini <contolini@users.noreply.github.com>
2024-10-23 16:18:49 -0400

    Add exports config to DS package
    The `exports` field allows multiple entrypoints to be defined instead of
    just a single `main` entrypoint. This allows the bundled code to be selectively
    imported by developers. For our purposes, we can package the majority of our DS
    code into a single file but keep the tooltip code separate. Tooltips are used
    on very few pages and they rely on a third party library that we don't want users
    to unnecessarily download on every page of cf.gov.
    
    See https://nodejs.org/api/packages.html#package-entry-points

--- packages/cfpb-design-system/src/components/cfpb-tooltips/index.js
+++ packages/cfpb-design-system/src/components/cfpb-tooltips/index.js
@@ -1,6 +1,79 @@
 /* ==========================================================================
-   Design System
-   Tooltips
+   Tooltip Organism
    ========================================================================== */
 
-export { Tooltip } from './tooltip.js';
+import tippy from 'tippy.js';
+import { instantiateAll } from '@cfpb/cfpb-design-system';
+
+import * as TooltipStyles from './tooltip.scss';
+
+const BASE_ATTRIBUTE = 'data-tooltip';
+
+/**
+ * Tooltip
+ * @class
+ * @classdesc Initializes a new Tooltip.
+ * @param {HTMLElement} element - The DOM element that should
+ *  activate the tooltip.
+ * @returns {Tooltip} An instance.
+ */
+function Tooltip(element) {
+  const tooltipContent = element.getAttribute(BASE_ATTRIBUTE);
+
+  /**
+   * Set up and create the tooltip.
+   * @returns {Tooltip} An instance.
+   */
+  function init() {
+    tippy(element, {
+      theme: 'cfpb',
+      maxWidth: 450,
+      content: function (reference) {
+        const template = reference.parentElement.querySelector(
+          `#${tooltipContent}`,
+        );
+        const container = document.createElement('div');
+        const node = document.importNode(template.content, true);
+        container.appendChild(node);
+        return container;
+      },
+      // See https://atomiks.github.io/tippyjs/v6/plugins/
+      plugins: [
+        {
+          name: 'hideOnEsc',
+          defaultValue: true,
+          fn({ hide }) {
+            /**
+             * Hide when the escape key is pressed.
+             * @param {KeyboardEvent} event - Key down event.
+             */
+            function onKeyDown(event) {
+              if (event.key === 'Escape') {
+                hide();
+              }
+            }
+            return {
+              onShow() {
+                document.addEventListener('keydown', onKeyDown);
+              },
+              onHide() {
+                document.removeEventListener('keydown', onKeyDown);
+              },
+            };
+          },
+        },
+      ],
+    });
+  }
+
+  // Attach public events.
+  this.init = init;
+
+  return this;
+}
+
+Tooltip.BASE_ATTRIBUTE = BASE_ATTRIBUTE;
+Tooltip.init = (scope) =>
+  instantiateAll(`[${Tooltip.BASE_ATTRIBUTE}]`, Tooltip, scope);
+
+export { Tooltip, TooltipStyles };

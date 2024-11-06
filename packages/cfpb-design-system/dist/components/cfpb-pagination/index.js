d6ea4c1480dfbf043a4dd0f37d59e73bbb9ed94f
Ans <git@anselmbradford.com>
2024-11-06 11:26:12 -0500

    Add `row-gap`

--- packages/cfpb-design-system/dist/components/cfpb-pagination/index.js
+++ packages/cfpb-design-system/dist/components/cfpb-pagination/index.js
@@ -1,2 +1,2 @@
-(()=>{var r=Object.defineProperty;var n=(i,a)=>{for(var t in a)r(i,t,{get:a[t],enumerable:!0})};var e={};n(e,{default:()=>o});var o={};})();
+(()=>{var i=Object.defineProperty;var n=(r,a)=>{for(var t in a)i(r,t,{get:a[t],enumerable:!0})};var e={};n(e,{default:()=>o});var o={};})();
 //# sourceMappingURL=index.js.map

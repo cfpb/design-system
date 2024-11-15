0cd1019a10ecb2a35776d26778cfe60b15511c04
Ans <git@anselmbradford.com>
2024-11-15 17:30:51 -0500

    Adds date input example and adjusts baseline appearance

--- packages/cfpb-design-system/dist/base/index.js
+++ packages/cfpb-design-system/dist/base/index.js
@@ -1,2 +1,2 @@
-(()=>{var a=Object.defineProperty;var o=(n,t)=>{for(var i in t)a(n,i,{get:t[i],enumerable:!0})};var e={};o(e,{default:()=>l});var l={};var h={};o(h,{default:()=>m});var m={};})();
+(()=>{var n=Object.defineProperty;var o=(a,t)=>{for(var i in t)n(a,i,{get:t[i],enumerable:!0})};var e={};o(e,{default:()=>l});var l={};var h={};o(h,{default:()=>m});var m={};})();
 //# sourceMappingURL=index.js.map

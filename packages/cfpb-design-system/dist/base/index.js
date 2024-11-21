c41101f6be4994c8532b884191525df269529a9d
Ans <git@anselmbradford.com>
2024-11-21 11:00:40 -0500

    More tweaks to wrangle aligning iOS with everyone else

--- packages/cfpb-design-system/dist/base/index.js
+++ packages/cfpb-design-system/dist/base/index.js
@@ -1,2 +1,2 @@
-(()=>{var n=Object.defineProperty;var o=(a,t)=>{for(var i in t)n(a,i,{get:t[i],enumerable:!0})};var e={};o(e,{default:()=>l});var l={};var h={};o(h,{default:()=>d});var d={};})();
+(()=>{var n=Object.defineProperty;var o=(a,t)=>{for(var i in t)n(a,i,{get:t[i],enumerable:!0})};var e={};o(e,{default:()=>l});var l={};var h={};o(h,{default:()=>m});var m={};})();
 //# sourceMappingURL=index.js.map

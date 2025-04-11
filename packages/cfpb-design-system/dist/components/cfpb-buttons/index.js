3327b255d8d85584c282cd07ecb0d5e77101a81a
Ans <git@anselmbradford.com>
2025-04-11 16:45:46 -0400

    Update secondary button appearance

--- packages/cfpb-design-system/dist/components/cfpb-buttons/index.js
+++ packages/cfpb-design-system/dist/components/cfpb-buttons/index.js
@@ -1,2 +1,2 @@
-(()=>{var s=Object.defineProperty;var a=(i,r)=>{for(var o in r)s(i,o,{get:r[o],enumerable:!0})};var n={};a(n,{default:()=>d});var d={};var t={};a(t,{default:()=>b});var b={};var e={};a(e,{default:()=>g});var g={};})();
+(()=>{var s=Object.defineProperty;var a=(i,r)=>{for(var o in r)s(i,o,{get:r[o],enumerable:!0})};var e={};a(e,{default:()=>d});var d={};var t={};a(t,{default:()=>b});var b={};var n={};a(n,{default:()=>g});var g={};})();
 //# sourceMappingURL=index.js.map

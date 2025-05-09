e982951436ebe8602e2101c4b37ad1f5c8b11e2b
Ans <git@anselmbradford.com>
2025-05-09 18:52:42 -0400

    Remove `respond-to-print` and set media query helpers to `only screen`

--- packages/cfpb-design-system/dist/base/index.js
+++ packages/cfpb-design-system/dist/base/index.js
@@ -1,2 +1,2 @@
-(()=>{var n=Object.defineProperty;var o=(a,t)=>{for(var i in t)n(a,i,{get:t[i],enumerable:!0})};var e={};o(e,{default:()=>l});var l={};var h={};o(h,{default:()=>d});var d={};})();
+(()=>{var a=Object.defineProperty;var o=(n,t)=>{for(var i in t)a(n,i,{get:t[i],enumerable:!0})};var e={};o(e,{default:()=>l});var l={};var h={};o(h,{default:()=>d});var d={};})();
 //# sourceMappingURL=index.js.map

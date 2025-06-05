9d9e14d88365668dcbefc0823b7553664cb6c567
Ans <git@anselmbradford.com>
2025-06-05 15:21:05 -0400

    Remove legacy form input CSS

--- packages/cfpb-design-system/dist/base/index.js
+++ packages/cfpb-design-system/dist/base/index.js
@@ -1,2 +1,2 @@
-(()=>{var r=Object.defineProperty;var o=(a,t)=>{for(var i in t)r(a,i,{get:t[i],enumerable:!0})};var e={};o(e,{default:()=>l});var l={};var h={};o(h,{default:()=>d});var d={};})();
+(()=>{var a=Object.defineProperty;var o=(r,t)=>{for(var i in t)a(r,i,{get:t[i],enumerable:!0})};var e={};o(e,{default:()=>l});var l={};var h={};o(h,{default:()=>d});var d={};})();
 //# sourceMappingURL=index.js.map

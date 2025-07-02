3c6a5e9cefa86821f117dafa652ef5f92333e42d
Ans <git@anselmbradford.com>
2025-07-02 14:46:10 -0400

    Add `@fontsource/source-sans-3`

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
+FONT_REGULAR="source-sans-3-latin-400-normal.woff2"
+FONT_DEMI="source-sans-3-latin-600-normal.woff2"
+
 # Add required webfonts locally to docs/fonts/ directory.
 mkdir ./docs/fonts/
-cd ./docs/fonts
-prefix="https://github.com/cfpb/consumerfinance.gov/blob/main/static.in/cfgov-fonts/fonts/"
-fonts=("2cd55546-ec00-4af9-aeca-4a3cd186da53.woff2" "627fbb5a-3bae-4cd9-b617-2f923e29d55e.woff2")
-for font in ${fonts[@]}; do
-  # Flags: s = silent, L = follow redirects.
-  curl -sL "${prefix}${font}?raw=true" > ${font}
-done
+cp ./node_modules/@fontsource/source-sans-3/files/$FONT_REGULAR ./docs/fonts/$FONT_REGULAR
+cp ./node_modules/@fontsource/source-sans-3/files/$FONT_DEMI ./docs/fonts/$FONT_DEMI
 cd ../../

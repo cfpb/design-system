da47dc7dc2220b18181c57d5b456091ee38c13c2
Ans <git@anselmbradford.com>
2025-07-03 15:24:11 -0400

    Switch to Source Sans Variable

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
-FONT_REGULAR="source-sans-3-latin-400-normal.woff2"
-FONT_SEMI_BOLD="source-sans-3-latin-600-normal.woff2"
-FONT_BOLD="source-sans-3-latin-700-normal.woff2"
+FONT_VARIABLE="source-sans-3-latin-wght-normal.woff2"
 
 # Add required webfonts locally to docs/fonts/ directory.
 mkdir ./docs/fonts/
-cp ./node_modules/@fontsource/source-sans-3/files/$FONT_REGULAR ./docs/fonts/$FONT_REGULAR
-cp ./node_modules/@fontsource/source-sans-3/files/$FONT_SEMI_BOLD ./docs/fonts/$FONT_SEMI_BOLD
-cp ./node_modules/@fontsource/source-sans-3/files/$FONT_BOLD ./docs/fonts/$FONT_BOLD
+cp ./node_modules/@fontsource-variable/source-sans-3/files/$FONT_VARIABLE ./docs/fonts/$FONT_VARIABLE
 cd ../../

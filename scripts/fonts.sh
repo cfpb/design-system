199f210b3e0bbed6b91c7c95bc9256f6cd631424
Ans <git@anselmbradford.com>
2025-07-08 10:30:26 -0400

    Update fonts.sh

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 FONT_VARIABLE="source-sans-3-latin-wght-normal.woff2"
+BASE_DIR="./docs/fonts"
 
 # Add required webfonts locally to docs/fonts/ directory.
-mkdir ./docs/fonts/
-cp ./node_modules/@fontsource-variable/source-sans-3/files/$FONT_VARIABLE ./docs/fonts/$FONT_VARIABLE
-cd ../../
+mkdir $BASE_DIR
+cp ./node_modules/@fontsource-variable/source-sans-3/files/$FONT_VARIABLE $BASE_DIR/$FONT_VARIABLE

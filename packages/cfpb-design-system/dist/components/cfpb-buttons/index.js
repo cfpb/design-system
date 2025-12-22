acc0cf0e0c6527e3e820ddbc28bda96e3124ba5a
itsmedavep <11078351+itsmedavep@users.noreply.github.com>
2025-12-22 10:25:09 -0600

    fixing duplicate mixin name reference in button-link

--- packages/cfpb-design-system/dist/components/cfpb-buttons/index.js
+++ packages/cfpb-design-system/dist/components/cfpb-buttons/index.js
@@ -1,2 +1,2 @@
-(()=>{var o=Object.defineProperty;var a=(c,d)=>{for(var i in d)o(c,i,{get:d[i],enumerable:!0})};var e={};a(e,{default:()=>l});var l={};var r={};a(r,{default:()=>f});var f={};var t={};a(t,{default:()=>s});var s={};})();
+(()=>{var o=Object.defineProperty;var a=(c,d)=>{for(var i in d)o(c,i,{get:d[i],enumerable:!0})};var e={};a(e,{default:()=>l});var l={};var r={};a(r,{default:()=>f});var f={};var t={};a(t,{default:()=>g});var g={};})();
 //# sourceMappingURL=index.js.map

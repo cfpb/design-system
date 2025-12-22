a7337c8f1b9a12ef5e63d6f5c66dd7661984d3cb
itsmedavep <11078351+itsmedavep@users.noreply.github.com>
2025-12-22 10:15:47 -0600

    fixing duplicate mixin name ref for u-btn-divider

--- packages/cfpb-design-system/dist/components/cfpb-buttons/index.js
+++ packages/cfpb-design-system/dist/components/cfpb-buttons/index.js
@@ -1,2 +1,2 @@
-(()=>{var o=Object.defineProperty;var a=(i,t)=>{for(var c in t)o(i,c,{get:t[c],enumerable:!0})};var e={};a(e,{default:()=>l});var l={};var r={};a(r,{default:()=>f});var f={};var d={};a(d,{default:()=>v});var v={};})();
+(()=>{var o=Object.defineProperty;var a=(c,d)=>{for(var i in d)o(c,i,{get:d[i],enumerable:!0})};var e={};a(e,{default:()=>l});var l={};var r={};a(r,{default:()=>f});var f={};var t={};a(t,{default:()=>s});var s={};})();
 //# sourceMappingURL=index.js.map

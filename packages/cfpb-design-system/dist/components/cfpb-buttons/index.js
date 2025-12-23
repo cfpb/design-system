7ec961bfd8276f39ec9091a5d8f5a82ae6108e4d
itsmedavep <11078351+itsmedavep@users.noreply.github.com>
2025-12-23 15:49:27 -0600

    fixing web component buttons
    encapsulating all styles for web component buttons in :host

--- packages/cfpb-design-system/dist/components/cfpb-buttons/index.js
+++ packages/cfpb-design-system/dist/components/cfpb-buttons/index.js
@@ -1,2 +1,2 @@
-(()=>{var n=Object.defineProperty;var a=(i,t)=>{for(var c in t)n(i,c,{get:t[c],enumerable:!0})};var e={};a(e,{default:()=>l});var l={};var r={};a(r,{default:()=>f});var f={};var d={};a(d,{default:()=>g});var g={};})();
+(()=>{var c=Object.defineProperty;var a=(n,d)=>{for(var i in d)c(n,i,{get:d[i],enumerable:!0})};var e={};a(e,{default:()=>l});var l={};var r={};a(r,{default:()=>f});var f={};var t={};a(t,{default:()=>g});var g={};})();
 //# sourceMappingURL=index.js.map

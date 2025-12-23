4e5f7c130d7d6fafaa4e4d0298250d8ac0a015bf
itsmedavep <11078351+itsmedavep@users.noreply.github.com>
2025-12-23 14:01:01 -0600

    Revert "fixing button divider on legacy components"
    This reverts commit 324bfbe0786ca66d64bd8db15b18089b4080a0bd.

--- packages/cfpb-design-system/dist/components/cfpb-buttons/index.js
+++ packages/cfpb-design-system/dist/components/cfpb-buttons/index.js
@@ -1,2 +1,2 @@
-(()=>{var o=Object.defineProperty;var a=(i,t)=>{for(var c in t)o(i,c,{get:t[c],enumerable:!0})};var e={};a(e,{default:()=>l});var l={};var r={};a(r,{default:()=>f});var f={};var d={};a(d,{default:()=>g});var g={};})();
+(()=>{var n=Object.defineProperty;var a=(i,t)=>{for(var c in t)n(i,c,{get:t[c],enumerable:!0})};var e={};a(e,{default:()=>l});var l={};var r={};a(r,{default:()=>f});var f={};var d={};a(d,{default:()=>g});var g={};})();
 //# sourceMappingURL=index.js.map

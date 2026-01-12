a42f4ec4875cc53cdd02c2275f5572ca6f1e654a
itsmedavep <itsmedavep@users.noreply.github.com>
2026-01-12 14:18:58 +0000

    Deploy preview for PR 2480 🛫

---
+++ packages/cfpb-design-system/pr-previews/pr-2480/components/cfpb-buttons/index.js
@@ -0,0 +1,2 @@
+(()=>{var c=Object.defineProperty;var a=(n,d)=>{for(var i in d)c(n,i,{get:d[i],enumerable:!0})};var e={};a(e,{default:()=>l});var l={};var r={};a(r,{default:()=>f});var f={};var t={};a(t,{default:()=>g});var g={};})();
+//# sourceMappingURL=index.js.map
---
+++ packages/cfpb-design-system/pr-previews/pr-2480/components/cfpb-icons/index.js
@@ -0,0 +1,2 @@
+(()=>{var d=Object.defineProperty;var t=(c,r)=>{for(var e in r)d(c,e,{get:r[e],enumerable:!0})};var a={};t(a,{default:()=>i});var i={};})();
+//# sourceMappingURL=index.js.map

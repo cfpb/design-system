69a12571481778be6544e623d0e98fb9e3a7e085
itsmedavep <11078351+itsmedavep@users.noreply.github.com>
2026-01-15 15:33:00 -0600

    update to SD config to deal with native Figma JSON export
    Summary:
    
    Builds one CSS file per token JSON under packages/cfpb-design-system/src/tokens.
    Uses DTCG $type for transforms; names are full-path kebab-case.
    Prefers hex colors, emits CSS v4 rgba() only when RGBA is present and hex is absent.
    Preserves Figma alias metadata as var(--...) with collision checks.
    Outputs tight comma spacing in CSS values without touching comments.

--- packages/cfpb-design-system/dist/components/cfpb-buttons/index.js
+++ packages/cfpb-design-system/dist/components/cfpb-buttons/index.js
@@ -1,2 +1,2 @@
-(()=>{var c=Object.defineProperty;var a=(n,d)=>{for(var o in d)c(n,o,{get:d[o],enumerable:!0})};var e={};a(e,{default:()=>l});var l={};var r={};a(r,{default:()=>f});var f={};var t={};a(t,{default:()=>v});var v={};})();
+(()=>{var c=Object.defineProperty;var a=(n,d)=>{for(var i in d)c(n,i,{get:d[i],enumerable:!0})};var e={};a(e,{default:()=>l});var l={};var r={};a(r,{default:()=>f});var f={};var t={};a(t,{default:()=>g});var g={};})();
 //# sourceMappingURL=index.js.map

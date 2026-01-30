66667a9baba2ea23910944f8459320c039120a2d
Owen Zed <owenzed@Owens-MacBook-Pro.local>
2026-01-30 18:38:57 -0500

    modified vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,19 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
-  root: './docs',
-  base: '/',
+  root: '.', // repo root
+  build: {
+    outDir: 'docs/assets/dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'docs/assets/src/main.js')
+      }
+    }
+  },
   resolve: {
     alias: {
       '@cfpb': path.resolve(__dirname, 'packages/design-system/src')
     }
-  },
-  server: {
-    port: 5173,
-    open: true
-  },
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true
   }
 });

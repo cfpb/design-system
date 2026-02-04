34e6ce6f7413459bd1789b8bfd4a781340dde0c0
contolini <contolini@users.noreply.github.com>
2026-02-04 18:12:08 +0000

    Deploy preview for PR 2512 🛫

---
+++ pr-preview/pr-2512/design-system/assets/js/admin/widgets/genericPreviewTemplate.js
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import ReactLiquid from './ReactLiquid';
+import template from '../../../../_includes/generic-content.html';
+
+export default class Preview extends Component {
+  render() {
+    const data = {
+      page: this.props.entry.toJS().data,
+    };
+    return (
+      <div>
+        <ReactLiquid template={template} data={data} html />
+      </div>
+    );
+  }
+}
+
+Preview.propTypes = {
+  entry: PropTypes.object,
+};
---
+++ pr-preview/pr-2512/design-system/assets/js/main.js
@@ -0,0 +1,118 @@
+import { toggleAllDetails, toggleDetails } from './toggle-details.js';
+import AnchorJS from 'anchor-js';
+import {
+  CfpbButton,
+  CfpbExpandable,
+  CfpbFileUpload,
+  CfpbFormAlert,
+  CfpbFormChoice,
+  CfpbFormSearch,
+  CfpbFormSearchInput,
+  CfpbLabel,
+  CfpbList,
+  CfpbListItem,
+  CfpbTagFilter,
+  CfpbTagTopic,
+  CfpbTagGroup,
+  CfpbPagination,
+  CfpbSelect,
+  Expandable,
+  ExpandableGroup,
+  Summary,
+  SummaryMinimal,
+  Multiselect,
+  FlyoutMenu,
+  AlphaTransition,
+  MaxHeightTransition,
+  MoveTransition,
+} from '@cfpb/cfpb-design-system';
+import { Tooltip } from '@cfpb/cfpb-design-system/tooltips';
+import Tabs from './tabs.js';
+import redirectBanner from './redirect-banner.js';
+import sidebar from './sidebar.js';
+export * as MainStyles from '../css/main.scss';
+
+redirectBanner.init();
+sidebar.init();
+
+const anchors = new AnchorJS();
+// Add anchors to all headings (except page title headings)
+anchors.add('h2:not(.title), h3, h4, h5');
+// Ensure there are no anchors in inconvenient places
+anchors.remove(`
+  .a-live__code h2,
+  .a-live__code h3,
+  .a-live__code h4,
+  .a-live__code h5,
+  .o-expandable__label,
+  #search-results h3
+`);
+
+CfpbButton.init();
+CfpbExpandable.init();
+CfpbFileUpload.init();
+CfpbFormAlert.init();
+CfpbFormChoice.init();
+CfpbFormSearch.init();
+CfpbFormSearchInput.init();
+CfpbLabel.init();
+CfpbList.init();
+CfpbListItem.init();
+CfpbTagFilter.init();
+CfpbTagTopic.init();
+CfpbTagGroup.init();
+CfpbSelect.init();
+CfpbPagination.init();
+Summary.init();
+SummaryMinimal.init();
+ExpandableGroup.init();
+Expandable.init();
+Multiselect.init();
+Tooltip.init();
+
+// Exporting these classes to the window so that the transition-patterns.md
+// page can use them in its code snippets.
+window.AlphaTransition = AlphaTransition;
+window.MoveTransition = MoveTransition;
+window.MaxHeightTransition = MaxHeightTransition;
+window.FlyoutMenu = FlyoutMenu;
+
+// Tabs show under the show/hide details button on a pattern.
+const tabsContainerDom = document.querySelectorAll(`.${Tabs.BASE_CLASS}`);
+if (tabsContainerDom.length > 0) {
+  let tabsInst;
+  for (let i = 0, len = tabsContainerDom.length; i < len; i++) {
+    tabsInst = new Tabs(tabsContainerDom[i]);
+    tabsInst.init();
+  }
+}
+const toggleAllBtn = document.querySelector('#toggle-details');
+const toggleBtns = document.querySelectorAll('.a-toggle__code button');
+
+/**
+ * @param {MouseEvent} event - The mouse event object from the click.
+ */
+function handleToggleAllClick(event) {
+  event.preventDefault();
+  toggleAllDetails(toggleAllBtn);
+}
+
+/**
+ * @param {MouseEvent} event - The mouse event object from the click.
+ */
+function handleToggleClick(event) {
+  const target = event.target;
+  toggleDetails(target);
+}
+
+if (toggleAllBtn) {
+  toggleAllBtn.addEventListener('click', handleToggleAllClick, false);
+
+  if (window.localStorage.getItem('toggleState') === 'hide') {
+    toggleAllDetails(toggleAllBtn);
+  }
+}
+
+for (let i = 0, len = toggleBtns.length; i < len; i++) {
+  toggleBtns[i].addEventListener('click', handleToggleClick, false);
+}

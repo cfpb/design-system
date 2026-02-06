48a5c1853dda3e3f73ccfd2c60a93b8478f9c3e1
itsmedavep <11078351+itsmedavep@users.noreply.github.com>
2026-02-06 07:42:48 -0600

    Tokens: update to SD config to support native Figma JSON export (#2485)
    * update to SD config to deal with native Figma JSON export
    
    Summary:
    
    Builds one CSS file per token JSON under packages/cfpb-design-system/src/tokens.
    Uses DTCG $type for transforms; names are full-path kebab-case.
    Prefers hex colors, emits CSS v4 rgba() only when RGBA is present and hex is absent.
    Preserves Figma alias metadata as var(--...) with collision checks.
    Outputs tight comma spacing in CSS values without touching comments.
    
    * JSON files
    
    * Update custom-props.json
    
    * Update CONTRIBUTING.md
    
    Adding information about the new token form and blurb about editing.

--- packages/cfpb-design-system/dist/components/cfpb-icons/index.js
+++ packages/cfpb-design-system/dist/components/cfpb-icons/index.js
@@ -1,2 +1,2 @@
-(()=>{var d=Object.defineProperty;var c=(t,r)=>{for(var e in r)d(t,e,{get:r[e],enumerable:!0})};var a={};c(a,{default:()=>l});var l={};})();
+(()=>{var d=Object.defineProperty;var t=(c,r)=>{for(var e in r)d(c,e,{get:r[e],enumerable:!0})};var a={};t(a,{default:()=>i});var i={};})();
 //# sourceMappingURL=index.js.map

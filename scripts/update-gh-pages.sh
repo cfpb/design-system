fa1e448837cfd9db8e4974dae99a7fc1b444ba01
Chris Contolini <contolini@users.noreply.github.com>
2026-02-10 14:02:16 -0500

    Migrate DS from Jekyll to Eleventy
    Add GH PR preview action
    
    Remove netlify config file
    
    Destructure Decap template props so that previews render properly
    
    Replace the react-liquid icon injection hack with an esbuild module
    
    Preserve the `pr-preview` dir when deploying to GH pages
    
    The PR preview was mysteriously disappearing. Turns out that if someone
    opens or touches an unrelated PR, it'll deploy to GH pages and wipe out
    the PR previews from other pull requests. So now we use rsync to
    preserve the `pr-preview` directory.
    
    Eleventy uses 'collections.all' instead of 'site.pages'
    
    Update lockfile after fixing merge conflict

--- scripts/update-gh-pages.sh
+++ scripts/update-gh-pages.sh
@@ -9,16 +9,26 @@ remote_name="origin"
 main_branch="main"
 target_branch="gh-pages"
 target_dir="docs/_site/design-system/"
+gh_pages_dir=""
+
+# Remove the worktree when the script exits, regardless if it succeeds or fails
+cleanup() {
+	if [ -n "${gh_pages_dir}" ] && [ -d "${gh_pages_dir}" ]; then
+		git worktree remove "${gh_pages_dir}" --force || true
+	fi
+}
+
+trap cleanup EXIT
 
 cd "$GITHUB_WORKSPACE"
 
 git fetch "$remote_name" "$target_branch"
-git checkout "$target_branch"
 git checkout "$main_branch"
 
-# Mount the github pages branch as a subdirectory
+# Mount the github pages branch as a separate worktree
 # See http://sangsoonam.github.io/2019/02/08/using-git-worktree-to-deploy-github-pages.html
-git worktree add "$target_dir" "$target_branch"
+gh_pages_dir="$(mktemp -d)"
+git worktree add "$gh_pages_dir" "$target_branch"
 
 # Install dependencies
 yarn install
@@ -27,11 +37,18 @@ yarn after-install
 # Build the design system website
 yarn build-decap
 
-# Remove the built Jekyll website from .gitignore
+# Remove the built Eleventy website from .gitignore
 sed -i '/_site/d' ./.gitignore
 
+# Don't delete the `pr-preview` folder (or git metadata)
+rsync -av --delete \
+	--exclude "pr-preview" \
+	--exclude ".git" \
+	--exclude ".github" \
+	"$target_dir"/. "$gh_pages_dir"/
+
 # Check to see if there are any changes to commit.
-cd "$target_dir"
+cd "$gh_pages_dir"
 
 if [ -z "$(git status --porcelain)" ]; then
 	echo "no local changes to commit"

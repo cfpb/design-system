1a5d3cb75efb628faa8f3354fdc585899c901a5c
Ans <anselmbradford@users.noreply.github.com>
2026-02-27 13:11:10 -0500

    Update linter patterns

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-## Run prettier. See ignored path in .prettierignore.
-yarn prettier "./**/*.{js,jsx,ts,tsx,md,css,less}" --write
+## Run prettier. See ignored paths in .prettierignore.
+yarn prettier "./**/*.{js,jsx,ts,tsx,md,css,scss}" --write
 
-## Run JS linting. See ignored path in .eslintignore.
+## Run JS linting. See ignored paths in eslint.config.js.
 yarn eslint "./{.,test,docs,packages}/**/*.js" --fix
 
-## Run CSS linting. See ignored path in .stylelintignore.
-yarn stylelint "{docs,packages}/**/*.scss" --fix
+## Run CSS linting. See ignored paths in .stylelintignore.
+yarn stylelint "{docs,packages}/**/*.{css,scss}" --fix

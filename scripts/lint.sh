fab67b0446ce62bf0eb404d6f083086164a2a176
itsmedavep <11078351+itsmedavep@users.noreply.github.com>
2026-03-11 08:40:13 -0500

    Adding linter GH action on PR to main (#2551)
    Checkout action  use v6
    
    Update lint.sh to test if CI=true and if so:
    set -euo pipefail in CI if statement and then have all lint actions in check mode
    
    Legacy yarn lint behavior stays the same
    
    Updated how we call the various linting features to use yarn exec to ensure binary usage

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-## Run prettier. See ignored paths in .prettierignore.
-yarn prettier "./**/*.{js,jsx,ts,tsx,md,css,scss}" --write
+if [ "${CI:-}" = "true" ]; then
+  set -euo pipefail
+  PRETTIER_FLAGS=("--check")
+  ESLINT_FLAGS=("")
+  STYLELINT_FLAGS=("")
+else
+  export FORCE_COLOR="${FORCE_COLOR:-1}"
+  PRETTIER_FLAGS=("--write")
+  ESLINT_FLAGS=("--fix")
+  STYLELINT_FLAGS=("--fix")
+fi
+
+## Run Prettier, See ignored paths in .prettierignore
+yarn exec prettier "./**/*.{js,jsx,ts,tsx,md,css,scss}" ${PRETTIER_FLAGS[@]}
 
 ## Run JS linting. See ignored paths in eslint.config.js.
-yarn eslint "./{.,test,docs,packages}/**/*.js" --fix
+yarn exec eslint "./{.,test,docs,packages}/**/*.js" ${ESLINT_FLAGS[@]}
 
 ## Run CSS linting. See ignored paths in .stylelintignore.
-yarn stylelint "{docs,packages}/**/*.{css,scss}" --fix
+yarn exec stylelint "{docs,packages}/**/*.{css,scss}" ${STYLELINT_FLAGS[@]}

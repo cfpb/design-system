1ba29bcc2d2e0fca430e04fd315d50750c6f2244
Ans <anselmbradford@users.noreply.github.com>
2026-04-10 15:12:01 -0400

    Remove cfpb-file-upload SCSS cruft

--- packages/cfpb-design-system/src/elements/cfpb-file-upload/index.js
+++ packages/cfpb-design-system/src/elements/cfpb-file-upload/index.js
@@ -1,6 +1,6 @@
 import { LitElement, html, css, unsafeCSS } from 'lit';
 import { ref, createRef } from 'lit/directives/ref.js';
-import styles from './styles.component.scss?inline';
+import styles from './styles.component.css?inline';
 import { CfpbButton } from '../cfpb-button';
 
 /**

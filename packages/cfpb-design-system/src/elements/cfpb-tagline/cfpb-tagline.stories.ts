f9f434e16a011703cccb6b0c762fbe6295430fb4
itsmedavep <11078351+itsmedavep@users.noreply.github.com>
2026-07-08 13:59:18 -0500

    tweaking cfpb-tagline story to include slot

--- packages/cfpb-design-system/src/elements/cfpb-tagline/cfpb-tagline.stories.ts
+++ packages/cfpb-design-system/src/elements/cfpb-tagline/cfpb-tagline.stories.ts
@@ -1,30 +1,30 @@
 /// <reference types="vite/client" />
-import { html } from 'lit';
-import type { Meta, StoryObj } from '@storybook/web-components';
+import type { Meta, StoryObj } from '@storybook/web-components-vite';
 import { getStorybookHelpers } from '@wc-toolkit/storybook-helpers';
 import type { CfpbTaglineProps } from '../../../../../storybook/custom-elements-types.d.ts';
 import { CfpbTagline } from './index.js';
 
 CfpbTagline.init();
 
-const { args, argTypes } = getStorybookHelpers<CfpbTaglineProps>(
+const { args, argTypes, template } = getStorybookHelpers<CfpbTaglineProps>(
   'cfpb-tagline',
   { excludeCategories: ['methods'] },
 );
 
-const meta: Meta<CfpbTaglineProps> = {
+type TagLineStoryArgs = CfpbTaglineProps & { 'default-slot'?: string };
+
+const meta: Meta<TagLineStoryArgs> = {
   title: 'Web Components/cfpb-tagline',
   component: 'cfpb-tagline',
   tags: ['autodocs'],
   args,
   argTypes,
-  render: ({ isLarge }) =>
-    html`<cfpb-tagline ?isLarge="${isLarge}"></cfpb-tagline>`,
+  render: (args) => template(args),
 };
 
 export default meta;
 
-type Story = StoryObj<CfpbTaglineProps>;
+type Story = StoryObj<TagLineStoryArgs>;
 
 export const Default: Story = {};
 

867af31658deacbb528387e371ab8eafcebf347d
itsmedavep <11078351+itsmedavep@users.noreply.github.com>
2026-08-05 09:51:29 -0500

    removed guard in boilerplate code

--- vitest.config.js
+++ vitest.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vitest/config';
 import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 import { playwright } from '@vitest/browser-playwright';
-const dirname =
-  typeof __dirname !== 'undefined'
-    ? __dirname
-    : path.dirname(fileURLToPath(import.meta.url));
+
+const dirname = import.meta.dirname;
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
